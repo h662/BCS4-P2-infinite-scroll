ad7a37b85ff3208ef21c5871004ea9329bd91e48
h662 <h662hong@gmail.com>
2023-11-21 08:45:49 +0900

    IntersectionObserver

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { useEffect, useRef } from "react";
+
+import A from "./components/A";
+import B from "./components/B";
+import C from "./components/C";
+import D from "./components/D";
+import Header from "./components/Header";
+
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  const bRef = useRef();
+  const observer = useRef();
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver((entries) => {
+      console.log(entries[0].isIntersecting);
+    });
+
+    observer.current.observe(bRef.current);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <A />
+      <B bRef={bRef} />
+      <C />
+      <D />
+    </>
+  );
 };
 
 export default App;
